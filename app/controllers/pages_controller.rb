da8c4060191dc8e74d021520607a09871e16bc2b
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-02-03 11:09:21 -0600

    Added pages_controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+
+  # GET /homepage
+  def homepage
+  end
+
+  # GET /about_us
+  def about_us
+  end
+
+  # GET /contact
+  def contact
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :students
   get '/list_students', to: 'students#index'
 
+  # Static pages
+  root 'pages#homepage'
+
+
+  get '/contact', to: 'pages#contact'
+  get '/about_us', to: 'pages#about_us'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
